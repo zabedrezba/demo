4b66551b383a595a349006db196e5a93157f8c80
Zabed Al Rezba <zabed.rezba@gmail.com>
2023-07-11 21:03:38 +0600

    Quiz commit

--- src/react-quiz/pages/Quiz.js
+++ src/react-quiz/pages/Quiz.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Quiz = () => {
     //const {id} = useParams();
